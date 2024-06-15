28a25c457fa1ce0889a07f072cdea1af37a91f68
pirosiki197 <lingmukangtai825@gmail.com>
2024-06-15 17:20:57 +0900

    impl PATCH EventConfirm

--- server/handler/events.go
+++ server/handler/events.go
@@ -1,7 +1,9 @@
 package handler
 
 import (
+	"database/sql"
 	"net/http"
+	"slices"
 
 	"github.com/google/uuid"
 	"github.com/traP-jp/h24s_10/api"
@@ -35,6 +37,7 @@ func (h *Handler) PostEvents(ctx echo.Context) error {
 		return echo.NewHTTPError(http.StatusInternalServerError, err)
 	}
 
+	// create date options
 	options := make([]model.EventDate, len(req.DateOptions))
 	for i, option := range req.DateOptions {
 		id, err := uuid.NewV7()
@@ -49,6 +52,20 @@ func (h *Handler) PostEvents(ctx echo.Context) error {
 		}
 	}
 	err = h.repo.CreateEventDates(options)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError, err)
+	}
+
+	// create targets
+	targets := make([]model.EventTarget, len(req.Targets))
+	for i, target := range req.Targets {
+		targets[i] = model.EventTarget{
+			ID:      uuid.New(),
+			EventID: eventID,
+			TraQID:  target,
+		}
+	}
+	err = h.repo.CreateEventTargets(targets)
 	if err != nil {
 		return echo.NewHTTPError(http.StatusInternalServerError, err)
 	}
@@ -96,7 +113,90 @@ func (h *Handler) GetEventsEventID(ctx echo.Context, eventID api.EventID) error 
 
 // (PATCH /events/{eventID}/confirm)
 func (h *Handler) PatchEventsEventIDConfirm(ctx echo.Context, eventID api.EventID) error {
-	return nil
+	var req api.PatchEventConfirmRequest
+	if err := ctx.Bind(&req); err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, err)
+	}
+
+	// check if the user is the host of the event
+	event, err := h.repo.GetEventByEventID(eventID)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError, err)
+	}
+	userID := ctx.Get(traQIDKey).(string)
+	if event.HostID != userID {
+		return echo.NewHTTPError(http.StatusForbidden, "you are not the host of this event")
+	}
+
+	event.IsConfirmed = req.IsConfirmed
+
+	if req.IsConfirmed {
+		if req.EventDateOptionID == nil {
+			return echo.NewHTTPError(http.StatusBadRequest, "event_date_option_id is required")
+		}
+		err = h.makeConfirmed(eventID, *req.EventDateOptionID, event)
+	} else {
+		err = h.makeUnconfirmed(event)
+	}
+	if err != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError, err)
+
+	}
+
+	return ctx.NoContent(http.StatusNoContent)
+}
+
+func (h *Handler) makeConfirmed(eventID uuid.UUID, eventDateID uuid.UUID, event model.Event) error {
+	// get all event dates of the event
+	eventDates, err := h.repo.GetEventDates(eventID)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError, err)
+	}
+	// find the event date option which the host selected
+	idx := slices.IndexFunc(eventDates, func(d model.EventDate) bool {
+		return d.ID == eventDateID
+	})
+	if idx == -1 {
+		return echo.NewHTTPError(http.StatusBadRequest, "invalid event date option")
+	}
+	eventDate := eventDates[idx]
+
+	// get all votes for the event date
+	votes, err := h.repo.GetEventDateVotes(eventDate.ID)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError, err)
+	}
+
+	// create participants
+	participants := make([]model.Participant, len(votes))
+	for i, vote := range votes {
+		id, err := uuid.NewV7()
+		if err != nil {
+			return echo.NewHTTPError(http.StatusInternalServerError, err)
+		}
+		participants[i] = model.Participant{
+			ID:      id,
+			EventID: eventID,
+			TraQID:  vote.TraQID,
+			TeamID:  1,
+		}
+	}
+	err = h.repo.CreateParticipants(participants)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError, err)
+	}
+
+	event.Start = sql.NullTime{Time: eventDate.Start, Valid: true}
+	event.End = sql.NullTime{Time: eventDate.End, Valid: true}
+
+	return h.repo.UpdateEvent(event)
+}
+
+func (h *Handler) makeUnconfirmed(event model.Event) error {
+	event.IsConfirmed = false
+	event.Start = sql.NullTime{}
+	event.End = sql.NullTime{}
+	return h.repo.UpdateEvent(event)
 }
 
 // (GET /events/{eventID}/participants)
