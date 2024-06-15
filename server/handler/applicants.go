79d95eb3c3e17575035c07c507834ae5121ea130
jippo <morimor1@icloud.com>
2024-06-15 14:52:22 +0900

    GET /events/{eventID}/applicantsを実装

--- server/handler/applicants.go
+++ server/handler/applicants.go
@@ -1,6 +1,10 @@
 package handler
 
 import (
+	"fmt"
+	"net/http"
+
+	"github.com/google/uuid"
 	"github.com/traP-jp/h24s_10/api"
 
 	"github.com/labstack/echo/v4"
@@ -8,7 +12,33 @@ import (
 
 // (GET /events/{eventID}/applicants)
 func (h *Handler) GetEventsEventIDApplicants(ctx echo.Context, eventID api.EventID) error {
-	return nil
+	eventDates, err := h.repo.GetEventDates(eventID)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError, err)
+	}
+
+	dateOptions := make(map[string][]uuid.UUID)
+	for _, date := range eventDates {
+		dateVotes, err := h.repo.GetEventDateVotes(date.ID)
+		if err != nil {
+			return echo.NewHTTPError(http.StatusInternalServerError, err)
+		}
+
+		for _, vote := range dateVotes {
+			dateOptions[vote.TraQID] = append(dateOptions[vote.TraQID], vote.DateID)
+		}
+	}
+
+	res := make(api.GetEventApplicantsResponse, 0, len(dateOptions))
+	for traQID, dateIDs := range dateOptions {
+		res = append(res, api.Applicant{
+			TraqID: &traQID,
+			DateOptionIDs: &dateIDs,
+		})
+	}
+	fmt.Println("デバッグ！！！！！！")
+	fmt.Println(res)
+	return ctx.JSON(http.StatusOK, res)
 }
 
 // (POST /events/{eventID}/applicants)
--- server/model/date_vote.go
+++ server/model/date_vote.go
@@ -2,20 +2,26 @@ package model
 
 import (
 	"errors"
+	"time"
 
 	"github.com/google/uuid"
 )
 
 type DateVote struct {
-	ID      uuid.UUID `db:"id"`
-	EventID uuid.UUID `db:"event_id"`
-	TraQID  string    `db:"traq_id"`
-	DateID  uuid.UUID `db:"event_date_id"`
-	Comment string    `db:"comment"`
+	ID        uuid.UUID `db:"id"`
+	EventID   uuid.UUID `db:"event_id"`
+	TraQID    string    `db:"traq_id"`
+	DateID    uuid.UUID `db:"event_date_id"`
+	CreatedAt time.Time `db:"created_at"`
 }
 
-func (repo *Repository) GetEventDateVotes(eventID uuid.UUID) ([]DateVote, error) {
-	return nil, errors.New("not implemented")
+func (repo *Repository) GetEventDateVotes(eventDateID uuid.UUID) ([]DateVote, error) {
+	dateVotes := make([]DateVote, 0)
+	err := repo.db.Select(&dateVotes, "SELECT * FROM date_votes WHERE event_date_id = ?", eventDateID)
+	if err != nil {
+		return nil, err
+	}
+	return dateVotes, nil
 }
 
 func (repo *Repository) CreateDateVotes(votes []DateVote) error {
