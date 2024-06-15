73c06383cac2903b3abfff9ce20f0906853f6aba
nokhnaton <sage_of_forest@icloud.com>
2024-06-15 16:12:17 +0900

    日付選択UIを改善

--- client/src/components/CreateEvent/DateSelector.vue
+++ client/src/components/CreateEvent/DateSelector.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { computed, ref } from "vue";
+import { computed, ref, watch } from "vue";
 import { format, parseISO, startOfDay } from "date-fns";
 
 const emit = defineEmits<{
@@ -16,6 +16,14 @@ const sortedDates = computed(() =>
 const startTime = ref(format(now, "HH:mm"));
 const endTime = ref(format(now, "HH:mm"));
 
+watch([startTime, endTime], () => {
+  console.log(startTime.value, endTime.value);
+  if (startTime.value && endTime.value && startTime.value > endTime.value) {
+    endTime.value = startTime.value;
+  }
+  console.log(startTime.value, endTime.value);
+});
+
 const updateValue = (value: Date[]) => {
   console.log(value, startTime.value, endTime.value);
 };
@@ -30,48 +38,56 @@ const dialog = ref(false);
 <template>
   <v-btn @click="dialog = !dialog"></v-btn>
   <v-dialog v-model="dialog" max-width="800" max-height="600">
-    <v-card>
-      <v-container class="d-flex flex-row justify-center">
+    <v-card class="h-100">
+      <v-container
+        class="d-flex justify-center height-100"
+        :class="$style.container"
+      >
         <div>
           <v-date-picker
             v-model="dates"
             label="日付"
             :allowedDates="allowedDates"
             multiple
+            :class="$style.datePicker"
           />
-          <v-btn block @click="dates = []">日付をクリア</v-btn>
+          <div>
+            <v-btn :class="$style.clear" @click="dates = []"
+              >日付をクリア</v-btn
+            >
+          </div>
         </div>
-        <v-container
-          class="w-50 d-flex flex-column justify-end py-0 px-0 mr-0 ml-4"
-          :class="$style.timeContainer"
-        >
-          <div :class="$style.scroll">
-            <div v-for="date in sortedDates" :key="date.toISOString()">
-              {{ format(date, "yyyy/MM/dd") }}
+        <div :class="$style.timeContainerWrap" class="w-50">
+          <v-container
+            class="d-flex flex-column justify-end py-0 px-0 mr-0 ml-4"
+            :class="$style.timeContainer"
+          >
+            <v-list :class="$style.scroll" class="hidden-sm-and-down">
+              <v-list-item
+                v-for="date in sortedDates"
+                :key="date.toISOString()"
+                :title="format(date, 'yyyy/MM/dd')"
+              />
+            </v-list>
+            <div :class="$style.timeRange">
+              <v-text-field v-model="startTime" label="開始時刻" type="time" />
+              <v-text-field v-model="endTime" label="終了時刻" type="time" />
             </div>
-          </div>
-          <div :class="$style.timeRange">
-            <v-text-field
-              v-model="startTime"
-              label="開始時刻"
-              type="time"
-              :min="new Date().toISOString()"
-            />
-            <v-text-field
-              v-model="endTime"
-              label="終了時刻"
-              type="time"
-              :min="new Date().toISOString()"
-            />
-          </div>
-          <v-btn @click="updateValue(dates)">OK</v-btn>
-        </v-container>
+            <v-btn @click="updateValue(dates)">OK</v-btn>
+          </v-container>
+        </div>
       </v-container>
     </v-card>
   </v-dialog>
 </template>
 
 <style module lang="scss">
+.container {
+  flex-direction: row;
+  @media (max-width: 800px) {
+    flex-direction: column;
+  }
+}
 .timeRange {
   display: flex;
   align-items: center;
@@ -81,7 +97,31 @@ const dialog = ref(false);
   flex: 1;
   overflow-y: auto;
 }
+.timeContainerWrap {
+  @media (max-width: 800px) {
+    padding-top: 16px;
+    padding-bottom: 16px;
+    margin: auto;
+  }
+  @media (min-width: 800px) {
+    margin-top: 0px;
+    max-height: 100%;
+    position: relative;
+  }
+}
 .timeContainer {
-  max-height: 100%;
+  @media (min-width: 800px) {
+    position: absolute;
+    top: 0;
+    bottom: 0;
+  }
+}
+
+.datePicker {
+  margin: auto;
+}
+.clear {
+  display: block;
+  margin: auto;
 }
 </style>
