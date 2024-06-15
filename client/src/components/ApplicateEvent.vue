e43eee26badb1d46f19c2aa49965f38edd2f89ba
ogu-kazemiya <135459827+ogu-kazemiya@users.noreply.github.com>
2024-06-15 18:40:17 +0900

    ApplicateEventで日時の選択を実装

--- client/src/components/ApplicateEvent.vue
+++ client/src/components/ApplicateEvent.vue
@@ -1,51 +1,71 @@
 <script setup lang="ts">
-import{ ref } from 'vue'
+import{ ref } from 'vue';
+import { format, parseISO, startOfDay } from "date-fns";
 
-type DateOption = string
-
+type DateOption = {
+  id: string;
+  start: Date;
+  end: Date;
+}
 type EventDetail = {
-  eventName: string;
+  id: string;
+  title: string;
   organizer: string;
   description: string;
+  isConfirmed: boolean;
   dateOptions: DateOption[];
 }
-type EntryStatus = { dateOption: DateOption, status: boolean }
 
+// exsample event
 const eventDetail: EventDetail = {
-  eventName: "イベント",
+  id: "eventID",
+  title: "eventTitle",
   organizer: "ogu_kazemiya",
-  description: "イベントの説明文",
-  dateOptions: ["1/1 10:00-", "1/1 11:00-", "1/2 10:00-"],
+  description: "eventDescription",
+  isConfirmed: false,
+  dateOptions: [{id: "dateOptionID",start: new Date(),end: new Date()}],
 }
 
-// const entryStatus: EntryStatus =
+const dateOptionIDs = ref<Date[]>([])
 
-const checkbox = ref<DateOption[]>([])
+const postDateOptionIDs = () => {
+  // DateOptionIDsをPOSTする
+}
 </script>
 
 <template>
   <div>
     <h1>イベント詳細</h1>
     <div>
-      <h2>{{ eventDetail.eventName }}</h2>
+      <h2>{{ eventDetail.title }}</h2>
       <div>
         by {{ eventDetail.organizer }}
         <img 
           :src="`https://q.trap.jp/api/v3/public/icon/${eventDetail.organizer}`"
           :alt="`${eventDetail.organizer}'s icon'`"
-          height="50" width="50" />
+          height="25" width="25" />
       </div>
       <div>{{ eventDetail.description }}</div>
-      <div v-for="dateOption in eventDetail.dateOptions" :key="dateOption">
-        <v-checkbox
-          v-model="checkbox"
-          :value="dateOption"
-          :label="dateOption"
-          hide-details>
-        </v-checkbox>
+
+      <div v-if="!eventDetail.isConfirmed">
+        <div v-for="dateOption in eventDetail.dateOptions" :key="dateOption.id">
+          <v-checkbox
+            v-model="dateOptionIDs"
+            :value="dateOption.id"
+            :label="`${format(dateOption.start, 'MM/dd(eee)')}
+              ${format(dateOption.start, 'HH:mm')} ~ ${format(dateOption.end, 'HH:mm')}`"
+            hide-details>
+          </v-checkbox>
+        </div>
+        <v-btn @click="postDateOptionIDs" color="green">保存</v-btn>
       </div>
-      <div>
-        {{ checkbox }}
+
+      <div v-if="eventDetail.isConfirmed">
+        <div>
+          {{ format(eventDetail.dateOptions[0].start, 'MM/dd(eee)') }}
+          {{ format(eventDetail.dateOptions[0].start, 'HH:mm') }} ~
+          {{ format(eventDetail.dateOptions[0].end, 'HH:mm') }}
+        </div>
       </div>
     </div>
   </div>
