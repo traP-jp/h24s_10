bd0f89ed14029eb76336fb53cb9c8d9ae9cc21cd
pirosiki197 <lingmukangtai825@gmail.com>
2024-06-15 14:47:56 +0900

    add implementation to handler

--- server/handler/events.go
+++ server/handler/events.go
@@ -65,6 +65,11 @@ func (h *Handler) GetEventsEventID(ctx echo.Context, eventID api.EventID) error 
 	return nil
 }
 
+// (PATCH /events/{eventID}/confirm)
+func (h *Handler) PatchEventsEventIDConfirm(ctx echo.Context, eventID api.EventID) error {
+	return nil
+}
+
 // (GET /events/{eventID}/participants)
 func (h *Handler) GetEventsEventIDParticipants(ctx echo.Context, eventID api.EventID) error {
 	participants, err := h.repo.GetParticipants(eventID)
