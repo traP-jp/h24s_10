06ed427efe652dbfbc7dd27fec724f33f44b8b75
Luftalian <springtechtree@gmail.com>
2024-06-15 10:32:50 +0900

    エラーメッセージ

--- server/model/targets.go
+++ server/model/targets.go
@@ -12,12 +12,18 @@ type (
 		EventID uuid.UUID `db:"event_id"`
 		UserID  uuid.UUID `db:"user_id"`
 	}
+
+	CreateTargetRequest struct {
+		ID      uuid.UUID
+		EventID uuid.UUID
+		UserID  uuid.UUID
+	}
 )
 
 func (r *Repository) GetTargetsByEventID(eventID uuid.UUID) ([]Target, error) {
 	var targets []Target
 	if err := r.db.Select(&targets, "SELECT * FROM targets WHERE event_id = $1", eventID); err != nil {
-		return nil, fmt.Errorf("failed to get targets: %w", err)
+		return nil, fmt.Errorf("failed to get targets by event ID: %w", err)
 	}
 	return targets, nil
 }
